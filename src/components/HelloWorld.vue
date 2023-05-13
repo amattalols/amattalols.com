a6dc2cdfba19aa5c14c81c34224a28f9a1fe14c9
Amatta Lols <amattalols@gmail.com>
2023-05-13 10:06:55 -0400

    Add vuetify

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+
+      <div class="text-h5 font-weight-light mb-n1">Welcome to</div>
+
+      <h1 class="text-h2 font-weight-bold">amattalols.com</h1>
+
+      <div class="py-14" />
+
+
+      <div class="text-body-2 font-weight-light mb-n1">powered by vuetify</div>
+
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
